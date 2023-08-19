abd9649bfdb45228bd414970804641f182f25e3f
gurpreet1999 <127664291+gurpreet1999@users.noreply.github.com>
2023-08-19 13:15:52 +0530

    ok

--- server.js
+++ server.js
@@ -16,8 +16,14 @@ mongoose
 
 
   app.use(cors({
-    origin:"http://video-streaming-app-delpoy.s3-website.ap-south-1.amazonaws.com/"
+    origin:"http://video-streaming-app-delpoy.s3-website.ap-south-1.amazonaws.com"
   }))
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://video-streaming-app-delpoy.s3-website.ap-south-1.amazonaws.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
